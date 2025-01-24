5db9a04b0e15826c955a17678938f65b0b66a4e3
Loïc CAILLE <loccaille@gmail.com>
2025-01-24 09:58:27 +0100

    level 5 done

--- lib/levels.ts
+++ lib/levels.ts
@@ -28,17 +28,18 @@ export const levels: Level[] = [
       [false, false, false, false, false, false, true, false, false],
       [false, false, false, false, false, false, true, false, false],
       [false, false, true, true, true, true, true, false, false],
-      [false, false, true, false, false, false, true, false, false],
-      [false, false, true, false, false, false, true, false, false],
+      [false, false, true, false, true, false, true, false, false],
+      [false, false, true, false, true, false, true, false, false],
+      [false, false, true, false, true, false, true, false, false],
       [false, false, true, true, true, true, true, false, false],
-      [false, false, true, false, false, false, false, false, false],
       [false, false, true, false, false, false, false, false, false],
       [false, false, true, false, false, false, false, false, false],
     ],
     start: { x: 2, y: 8 },
     end: { x: 6, y: 0 },
     mustGoThrough: [
-      { x: 6, y: 5 },
+      { x: 6, y: 6 },
+      { x: 2, y: 2 },
     ], // Modifiez cette ligne pour définir plusieurs cases
   },
   // Add more levels here
