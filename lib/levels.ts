a7527e081330298b47959b7300357addb55b9298
Loïc CAILLE <loccaille@gmail.com>
2025-01-24 09:54:03 +0100

    level 4 done

--- lib/levels.ts
+++ lib/levels.ts
@@ -2,48 +2,44 @@ import { Level } from "@/types/game";
 
 export const levels: Level[] = [
   {
-    id: 1,
-    size: 8,
+    id: 4,
+    size: 9,
     grid: [
-      [true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true],
-      [true, true, true, true, true, true, true, true],
+      [false, false, false, false, false, false, true, false, false],
+      [false, false, false, false, false, false, true, false, false],
+      [false, false, true, true, true, true, true, false, false],
+      [false, false, true, false, false, false, true, false, false],
+      [false, false, true, false, false, false, true, false, false],
+      [false, false, true, true, true, true, true, false, false],
+      [false, false, true, false, false, false, false, false, false],
+      [false, false, true, false, false, false, false, false, false],
+      [false, false, true, false, false, false, false, false, false],
     ],
-    start: { x: 2, y: 4 },
-    end: { x: 7, y: 0 },
-    mirrorStart: { x: 5, y: 3 },
-    mirrorEnd: { x: 7, y: 7 },
-    mirrorAxis: "vertical",
+    start: { x: 2, y: 8 },
+    end: { x: 6, y: 0 },
     mustGoThrough: [
-      { x: 2, y: 0 },
-      { x: 2, y: 7 },
+      { x: 6, y: 5 },
+    ], // Modifiez cette ligne pour définir plusieurs cases
+  },
+  {
+    id: 4,
+    size: 9,
+    grid: [
+      [false, false, false, false, false, false, true, false, false],
+      [false, false, false, false, false, false, true, false, false],
+      [false, false, true, true, true, true, true, false, false],
+      [false, false, true, false, false, false, true, false, false],
+      [false, false, true, false, false, false, true, false, false],
+      [false, false, true, true, true, true, true, false, false],
+      [false, false, true, false, false, false, false, false, false],
+      [false, false, true, false, false, false, false, false, false],
+      [false, false, true, false, false, false, false, false, false],
+    ],
+    start: { x: 2, y: 8 },
+    end: { x: 6, y: 0 },
+    mustGoThrough: [
+      { x: 6, y: 5 },
     ], // Modifiez cette ligne pour définir plusieurs cases
   },
   // Add more levels here
-  {
-    id: 2,
-    size: 6,
-    grid: [
-      [true, true, true, true, true, false],
-      [true, false, true, false, true, false],
-      [true, false, true, false, true, true],
-      [true, false, true, false, false, false],
-      [true, false, true, false, true, true],
-      [true, true, true, true, true, true],
-    ],
-    start: { x: 0, y: 0 },
-    end: { x: 4, y: 0 },
-    mirrorStart: { x: 0, y: 3 },
-    mirrorEnd: { x: 7, y: 7 },
-
-    mustGoThrough: [
-      { x: 2, y: 0 },
-      { x: 2, y: 7 },
-    ], // Modifiez cette ligne pour définir plusieurs cases
-  },
 ];
