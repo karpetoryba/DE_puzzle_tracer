98089ef9e7e0aaa30eb23b6185956c3b2c2ee351
Loïc CAILLE <loccaille@gmail.com>
2025-01-25 18:02:11 +0100

    typo/removed double class

--- components/game/game_ui/grid/MazeGrid.tsx
+++ components/game/game_ui/grid/MazeGrid.tsx
@@ -188,7 +188,7 @@ export function MazeGrid({
     <div className="relative inline-block">
       <canvas
         ref={canvasRef}
-        className="absolute top-0 left-0 pointer-events-none z-10 pointer-events-none"
+        className="absolute top-0 left-0 z-10 pointer-events-none"
         width={level.size * (CELL_SIZE + 2)} // Adjust the width based on cell size and gap
         height={level.size * (CELL_SIZE + 2)} // Adjust the height based on cell size and gap
       />
